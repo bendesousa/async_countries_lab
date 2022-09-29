9097b3df39b65a0fc2cdb09207ddd67a1685c11c
bendesousa <bdsousa069@gmail.com>
2022-09-29 09:19:44 +0100

    fixed code with some help

--- app.js
+++ app.js
@@ -5,9 +5,8 @@ const fetchAllCountries = async () => {
     
     const response = await fetch("https://restcountries.com/v3.1/all");
     const jsonData = await response.json();
-    const countriesData = jsonData.data;
 
-    const countryNames = countriesData.map((name) => name.common);
+    const countryNames = jsonData.map((country) => country.name.common);
     countryNames.forEach((countryName)=> {
         const nameLi = document.createElement("li");
         nameLi.textContent = countryName;
@@ -16,33 +15,5 @@ const fetchAllCountries = async () => {
     
 }
 
-fetchAllCountries;
+fetchAllCountries();
 
-// const getAllCountries = async () => {
-//     const countriesUrls = [];
-//     for(let i = 1; i < 251; i++){
-//         countriesUrls.push(`https://restcountries.com/v3.1/name/${i}`);
-//     }
-
-//     const countriesPromises = countriesUrls.map( async (url) => {
-//         const response = await fetch(url);
-//         return response.json();
-//     })
-
-//     Promise.all( countriesPromises )
-//     .then((allResults) => {
-
-//         const countriesData = allResults.map( (result) => result.data ).flat();
-        
-//         const countryNames = countriesData.map((name) => name.common); 
-
-        
-
-//         countryNames.forEach((countryName) => {
-//             const nameLi = document.createElement("li");
-//             nameLi.textContent = countryName;
-//             countriesList.appendChild(nameLi);
-//         })
-
-//     })
-// }
